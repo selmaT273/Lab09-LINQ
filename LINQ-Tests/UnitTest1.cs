cfc1d96331f446039a57da0216c3d649f2e5fa05
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-14 16:58:12 -0500

    can read file test passing

--- LINQ-Tests/UnitTest1.cs
+++ LINQ-Tests/UnitTest1.cs
@@ -1,14 +1,34 @@
 using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
 using Xunit;
+using static Lab09_LINQ.GetJsonTypes;
 
 namespace LINQ_Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanReadFile()
         {
+            string filename = "data.json";
+            string data = File.ReadAllText(filename);
 
+            Assert.NotEmpty(data);
+
+            RootObject geojson = JsonConvert.DeserializeObject<RootObject>(data);
+
+            Assert.NotNull(geojson);
+
+            Assert.Equal("10001", geojson.features.First().properties.zip);
+            Assert.NotEmpty(
+                geojson.features
+                    .Select(feature => feature.properties.zip)
+                    .Distinct()
+                );
+
+            Assert.Equal(147, geojson.features.Count());
         }
     }
 }
